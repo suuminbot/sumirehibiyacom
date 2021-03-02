509d6946ffc65af8f9540cc5cee81fd1ac6cc639
Sumire Hibiya <sumire.hibiya@gmail.com>
2021-03-02 18:34:58 +0900

    update color setting

--- components/layout.js
+++ components/layout.js
@@ -9,7 +9,7 @@ export default function Layout({ children }) {
       <div className="py-3 md:p-6 relative">
         <div className="h-3 md:h-6 bg-white w-full fixed top-0 z-30" />
         <div className="h-3 md:h-6 bg-white w-full fixed bottom-0 z-30" />
-        <div className="bg-gray-800">
+        <div className="bg-gray-100 dark:bg-gray-800">
           <Header />
           <main>{children}</main>
           <Footer />
