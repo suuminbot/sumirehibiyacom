67478fbd63a879e2cd97df3051b78e653199657a
Sumire Hibiya <sumire.hibiya@gmail.com>
2024-01-05 16:07:22 +0900

    HomeからNotesを分離

---
+++ app/notes/page.tsx
@@ -0,0 +1,81 @@
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+import Link from 'next/link'
+import Image from 'next/image'
+import type { Metadata } from 'next'
+import PageTitle from '../components/pageTitle'
+
+export const metadata: Metadata = {
+  title: 'sumirehibiya.com',
+  openGraph: {
+    images: ['/og.png'],
+  },
+}
+
+export default async function Home() {
+  // contentディレクトリ内のマークダウンファイル一覧を取得
+  const postsDirectory = path.join(process.cwd(), 'content/notes')
+  const fileNames = fs.readdirSync(postsDirectory)
+
+  // 各ファイルの中身を取得
+  const posts = await Promise.all(
+    // 各ファイル情報を取得
+    fileNames.map(async (fileName) => {
+      const filePath = path.join(postsDirectory, fileName)
+      const fileContents = fs.readFileSync(filePath, 'utf8')
+      const { data } = matter(fileContents)
+      const date = new Date(data.date) // 記事の日付
+      const d = `${date.getFullYear()}年${('0' + (date.getMonth() + 1)).slice(
+        -2
+      )}月${('0' + date.getDate()).slice(-2)}日`
+
+      // slugとfrontmatter(title, date, description)を取得
+      return {
+        slug: fileName.replace('.md', ''),
+        frontmatter: data,
+        d: d,
+      }
+    })
+  ).then((posts) =>
+    // 最新日付順に並び替え
+    posts.sort((a, b) =>
+      new Date(b.frontmatter.date) > new Date(a.frontmatter.date) ? 1 : -1
+    )
+  )
+
+  return (
+    <div>
+      <PageTitle text="Notes" />
+      <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-3">
+        {posts.map((post) => (
+          <div key={post.slug} className="flex mb-10 items-center">
+            <Link
+              href={`/notes/${post.slug}`}
+              className="mr-6 w-20 h-20 md:w-32 md:h-32 flex-none relative flex justify-center items-center hover:no-underline"
+            >
+              <p className="z-10 p-4 bg-white rounded">
+                {post.frontmatter.icon}
+              </p>
+              <Image
+                src={`${post.frontmatter.coverImage}`}
+                alt={`${post.frontmatter.title}`}
+                fill
+                className="object-cover"
+              />
+            </Link>
+            <div>
+              <Link
+                href={`/notes/${post.slug}`}
+                className="text-sm font-bold block mb-2"
+              >
+                {post.frontmatter.title}
+              </Link>
+              <p className="text-xs">{post.d}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
-import fs from 'fs'
-import path from 'path'
-import matter from 'gray-matter'
 import Link from 'next/link'
-import Image from 'next/image'
+// import Image from 'next/image'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
@@ -12,64 +9,40 @@ export const metadata: Metadata = {
   },
 }
 
-export default async function Home() {
-  // contentディレクトリ内のマークダウンファイル一覧を取得
-  const postsDirectory = path.join(process.cwd(), 'content/notes')
-  const fileNames = fs.readdirSync(postsDirectory)
-
-  // 各ファイルの中身を取得
-  const posts = await Promise.all(
-    // 各ファイル情報を取得
-    fileNames.map(async (fileName) => {
-      const filePath = path.join(postsDirectory, fileName)
-      const fileContents = fs.readFileSync(filePath, 'utf8')
-      const { data } = matter(fileContents)
-      const date = new Date(data.date) // 記事の日付
-      const d = `${date.getFullYear()}年${('0' + (date.getMonth() + 1)).slice(
-        -2
-      )}月${('0' + date.getDate()).slice(-2)}日`
-
-      // slugとfrontmatter(title, date, description)を取得
-      return {
-        slug: fileName.replace('.md', ''),
-        frontmatter: data,
-        d: d,
-      }
-    })
-  ).then((posts) =>
-    // 最新日付順に並び替え
-    posts.sort((a, b) =>
-      new Date(b.frontmatter.date) > new Date(a.frontmatter.date) ? 1 : -1
-    )
-  )
-
+export default function Home() {
   return (
-    <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-3">
-      {posts.map((post) => (
-        <div key={post.slug} className="flex mb-10 items-center">
-          <Link
-            href={`/notes/${post.slug}`}
-            className="mr-6 w-20 h-20 md:w-32 md:h-32 flex-none relative flex justify-center items-center hover:no-underline"
-          >
-            <p className="z-10 p-4 bg-white rounded">{post.frontmatter.icon}</p>
-            <Image
-              src={`${post.frontmatter.coverImage}`}
-              alt={`${post.frontmatter.title}`}
-              fill
-              className="object-cover"
-            />
-          </Link>
+    <div className="w-1/2 m-auto">
+      <div className="mb-24">
+        <p className="mb-2 text-2xl font-display !leading-tight">
+          Product designer crafting solutions at the intersection of user
+          problem-solving and business outcomes.
+        </p>
+        <p className="mb-2 !leading-relaxed">
+          I'm Sumire Hibiya, a Tokyo based product designer with ten years of
+          experience. I'm currently working at{' '}
+          <Link href="https://10x.co.jp/" target="_blank">
+            10X
+          </Link>{' '}
+          which is developing a product “Stailer” to establish and operate an
+          online grocery store for retail businesses.
+        </p>
+        <p className="!leading-relaxed">
+          I value deeply understanding user, prototyping, design systems and
+          team communications.
+        </p>
+        <Link href="/about" />
+      </div>
+      <h2 className="font-display">Activities</h2>
+      <div>
+        {/* card */}
+        <div className="flex">
           <div>
-            <Link
-              href={`/notes/${post.slug}`}
-              className="text-sm font-bold block mb-2"
-            >
-              {post.frontmatter.title}
-            </Link>
-            <p className="text-xs">{post.d}</p>
+            {' '}
+            <p>date</p>
+            <p>content</p>
           </div>
         </div>
-      ))}
+      </div>
     </div>
   )
 }
