b414d329231c82bdc16e8396bdffe03f3fc0a010
Sumire Hibiya <sumire.hibiya@gmail.com>
2024-01-06 08:37:11 +0900

    update home design

--- app/home/profile.tsx
+++ app/home/profile.tsx
@@ -1,26 +1,37 @@
+import Image from 'next/image'
 import Link from 'next/link'
 
 export default function Profile() {
   return (
-    <div className="mb-24">
-      <p className="mb-8 text-2xl font-display !leading-tight">
-        Product designer crafting solutions at the intersection of user
-        problem-solving and business outcomes.
-      </p>
-      <p className="mb-4 text-base !leading-relaxed">
-        I'm Sumire Hibiya, a Tokyo based product designer with ten years of
-        experience. I'm currently working at{' '}
-        <Link href="https://10x.co.jp/" target="_blank">
-          10X
-        </Link>
-        which is developing a product “Stailer” to establish and operate an
-        online grocery store for retail businesses.
-      </p>
-      <p className="text-base !leading-relaxed">
-        I value deeply understanding user, prototyping, design systems and team
-        communications.
-      </p>
-      <Link href="/about" />
+    <div className="flex items-start">
+      <Image
+        src="/profile@2x.png"
+        alt="Sumire Hibiya"
+        width="96"
+        height="96"
+        className="mr-4 border-4 border-white rounded-full"
+      />
+      <div>
+        <div className="mb-4">
+          <h1 className="mb-1 font-display text-lg !leading-none font-bold">
+            Sumire Hibiya
+          </h1>
+          <p className="text-sm text-stone-500">@suuminbot</p>
+        </div>
+        <div>
+          <p className="mb-2">
+            Product designer crafting solutions at the intersection of user
+            problem-solving and business outcomes. <br />
+            I'm currently working at
+            <Link href="https://10x.co.jp/" target="_blank">
+              10X
+            </Link>
+            which is developing a product “Stailer” to establish and operate an
+            online grocery store for retail businesses.
+          </p>
+          <Link href="/about">Read full profile</Link>
+        </div>
+      </div>
     </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -10,20 +10,38 @@ export const metadata: Metadata = {
   },
 }
 
-const Home: React.FC = () => (
-  <div className="w-1/2 m-auto">
-    <Profile />
-    <h2 className="font-display">Activities</h2>
-    {ActivitiesData.map((activity) => (
-      <ActivityCard
-        key={activity.title}
-        category={activity.category}
-        title={activity.title}
-        date={activity.date}
-        url={activity.url}
-      />
-    ))}
-  </div>
-)
+const Home: React.FC = () => {
+  // ActivitiesData を最新の5件だけ取得
+  const latestActivities = ActivitiesData.slice(0, 5)
+
+  return (
+    <div className="m-auto w-auto md:w-4/5 lg:w-3/5">
+      <div className="mb-16">
+        <Profile />
+      </div>
+      <div className="border-l-2 pl-6 relative">
+        <h2
+          className="
+            before:block before:h-8 before:w-8 before:bg-white before:absolute before:-left-5
+            font-display
+            flex items-center
+           "
+        >
+          <p className="w-2 h-2 rounded-full bg-stone-400 -ml-8"></p>
+          <p>2024</p>
+        </h2>
+        {latestActivities.map((activity) => (
+          <ActivityCard
+            key={activity.title}
+            category={activity.category}
+            title={activity.title}
+            date={activity.date}
+            url={activity.url}
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
 
 export default Home
